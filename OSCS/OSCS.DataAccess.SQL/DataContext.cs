fc55fd6be7cf38f72038fe9832a4875745a3242f
UtayaSurian <utayasurian97@gmail.com>
2019-05-05 01:17:34 +0800

    Initialized SQL Server along with Entity Framework

---
+++ OSCS/OSCS.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using OSCS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCS.DataAccess.SQL
+{
+    public class DataContext : DbContext //Base methods that can help us Entity Frameworks
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        //Creating model for SQL to create table
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
---
+++ OSCS/OSCS.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace OSCS.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OSCS.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OSCS.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
