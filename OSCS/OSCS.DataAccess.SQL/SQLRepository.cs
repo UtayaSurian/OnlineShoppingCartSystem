b9cba806d07608975c7c57bac153765b070bfb42
UtayaSurian <utayasurian97@gmail.com>
2019-05-05 11:28:34 +0800

    Implemented SQL Repository

---
+++ OSCS/OSCS.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,62 @@
+﻿using OSCS.Core.Contracts;
+using OSCS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCS.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity   //Use Generic
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;        //To access the nderline table for data context
+
+        //Passing in DataCOntext
+        //Inject the datacontext model
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();         //set the underline table based on using model <T>
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+            {
+                dbSet.Attach(t);
+
+                dbSet.Remove(t);
+            }
+        }
+
+        public T Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);    //Attach the passing object called t to the entity framework table
+            context.Entry(t).State = EntityState.Modified;  //To look for the save changes 
+        }
+    }
+}
--- OSCS/OSCS.WebUI/App_Start/UnityConfig.cs
+++ OSCS/OSCS.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using OSCS.Core.Contracts;
 using OSCS.Core.Models;
 using OSCS.DataAccess.InMemory;
+using OSCS.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace OSCS.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
