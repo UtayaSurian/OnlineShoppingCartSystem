436c8d5288e7e840306dbb60cff64207f78e5276
UtayaSurian <utayasurian97@gmail.com>
2019-05-05 15:23:17 +0800

    Added Product List

--- OSCS/OSCS.WebUI/Controllers/HomeController.cs
+++ OSCS/OSCS.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OSCS.Core.Contracts;
+using OSCS.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,35 @@ namespace OSCS.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        //Instances
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories; //Load from database
+
+        //Enable the repository which implements Irepository
+        //Refer it from IRepository interface class in OSCS.Core
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoriesContext)
+        {
+            //initiliaze
+            context = productContext;
+            productCategories = productCategoriesContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList(); //Get the products and store into list
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
