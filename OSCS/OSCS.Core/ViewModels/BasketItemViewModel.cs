0f8e6b15726892f45975f56097744f8d3e5a3c94
UtayaSurian <utayasurian97@gmail.com>
2019-05-05 22:22:02 +0800

    Finished Basket Service

---
+++ OSCS/OSCS.Core/ViewModels/BasketItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCS.Core.ViewModels
+{
+    public class BasketItemViewModel
+    {   //Refering from Basket Table
+        public string Id { get; set; }  //product id
+        public int Quantity { get; set; }   //product quantity
+        //Referring from product table
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+     
+    }
+}
--- OSCS/OSCS.Services/BasketService.cs
+++ OSCS/OSCS.Services/BasketService.cs
@@ -1,5 +1,6 @@
 ﻿using OSCS.Core.Contracts;
 using OSCS.Core.Models;
+using OSCS.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace OSCS.Services
 {
     //Read cookies from user
     //Creating,adding and removing from baskets
-    public class BasketService
+    public class BasketService : IBasketService
     {
         IRepository<Product> productContext;
         IRepository<Basket> basketContext;      //Load the basket and basket items
@@ -119,5 +120,58 @@ namespace OSCS.Services
                 basketContext.Commit();
             }
         }
+
+        public List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext)
+        {
+            //Get the basket from the database
+            Basket basket = GetBasket(httpContext, false);
+
+            if (basket != null)
+            {
+                //Query the product table and basket item table and select things needed to show: Inner Join using LINQ
+                var results = (from b in basket.BasketItems
+                              join p in productContext.Collection() on b.ProductId equals p.Id
+                              select new BasketItemViewModel()
+                              {
+                                  Id = b.Id,
+                                  Quantity = b.Quantity,
+                                  ProductName = p.Name,
+                                  Image = p.Image,
+                                  Price = p.Price
+                              }
+                              ).ToList();
+                return results;
+            }
+            else
+            {
+                //If nothng in basket, then return a new empty list of the basket items
+                return new List<BasketItemViewModel>();
+            }
+        }
+
+        public BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);  //If empty then not required to create basket
+            BasketSummaryViewModel model = new BasketSummaryViewModel(0,0); //Set by default by 0 items n total in basket 
+
+            //If any items currently exist then sum the quanutity which newly added by customer and calculate for total price
+            if(basket!=null)
+            {   //int? is when empty the it will send a null value such as quantity and price
+                int? basketCount = (from item in basket.BasketItems     //querying the basket items table
+                                    select item.Quantity).Sum();
+                decimal? basketTotal = (from item in basket.BasketItems
+                                        join p in productContext.Collection() on item.ProductId equals p.Id
+                                        select item.Quantity * p.Price).Sum();
+
+                model.BasketCount = basketCount ?? 0; //if the basket is empty then return with 0 by default
+                model.BasketTotal = basketTotal ?? decimal.Zero; //if the basket is empty then return with decimal 0 by default
+                return model;
+            }
+            else
+            {
+                return model;
+            }
+        }
+
     }
 }
