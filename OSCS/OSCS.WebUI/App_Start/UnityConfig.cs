387dc3a35d7ad6febb728a5d5628bb3634520d82
UtayaSurian <utayasurian97@gmail.com>
2019-05-06 00:51:23 +0800

    Created Basket UI

--- OSCS/OSCS.WebUI/App_Start/UnityConfig.cs
+++ OSCS/OSCS.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using OSCS.Core.Contracts;
 using OSCS.Core.Models;
 using OSCS.DataAccess.InMemory;
 using OSCS.DataAccess.SQL;
+using OSCS.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace OSCS.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
---
+++ OSCS/OSCS.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+﻿using OSCS.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSCS.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        //Add the implement service in order to create its methods under controllers
+        IBasketService basketService;
+
+        //To inject the basket service
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketService = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = basketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            basketService.AddToBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            basketService.RemoveFromBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        //Summary to get the basket summary result
+        //Refer BasketService under contract in DataAccess.SQL
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketService.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+    }
+}
